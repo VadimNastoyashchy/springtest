afded45a2407b978e5c72b78709364402e7a2045
vadymnastoiashchyi <vadim.89@ukr.net>
2020-08-19 11:25:17 +0300

    Add - HTML views (user-list, user-create, user-update) - Update UserController

--- src/main/java/com/myapp/springtest/controller/UserController.java
+++ src/main/java/com/myapp/springtest/controller/UserController.java
@@ -2,8 +2,11 @@ package com.myapp.springtest.controller;
 
 import com.myapp.springtest.model.User;
 import com.myapp.springtest.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -13,11 +16,28 @@ import java.util.List;
 
 @Controller
 public class UserController {
-    private UserService userService;
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/users")
-    public String findAll(){
+    public String findAll(Model model){
         List<User> userList = userService.findAll();
-        return "";
+        model.addAttribute("users", userList);
+        return "user-list";
+    }
+
+    @GetMapping("/user-create")
+    public String createUserForm(User user){
+        return "/user-create";
+    }
+
+    @PostMapping("/user-create")
+    public String crateUser(User user){
+        userService.saveUser(user);
+        return "redirect:/users";
     }
 }
